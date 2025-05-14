d072c5cbca23981f9c1c86e1c2c1329b004383f3
Oddbjørn Rønnestad <60390653+oddib@users.noreply.github.com>
2025-05-14 15:26:06 +0200

    testing for kernel 6.14

--- samsung/galaxybook-2-pro/default.nix
+++ samsung/galaxybook-2-pro/default.nix
@@ -1,23 +1,26 @@
-{ lib, pkgs, config, ... }: {
-  imports = [ ../../common/cpu/intel/alder-lake ];
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [../../common/cpu/intel/alder-lake];
 
   ###
   # Kernel module by Joshua Grisham, added features in readme
   # https://github.com/joshuagrisham/samsung-galaxybook-extras
   # deactivate with:
   # hardware.samsung-galaxybook.enable =  false;
-  # 
+  #
 
-   boot.extraModulePackages = [
-     (pkgs.callPackage ./kmod.nix {
-       kernel = config.boot.kernelPackages.kernel;
-     })
-   ];
-   boot.kernelModules = [ "samsung-galaxybook" "kvm-intel" ];
+  #  boot.extraModulePackages = [
+  #    (pkgs.callPackage ./kmod.nix {
+  #      kernel = config.boot.kernelPackages.kernel;
+  #    })
+  #  ];
+  boot.kernelModules = ["kvm-intel"];
 
-  boot.kernelParams = [ "i915.enable_dpcd_backlight=3" ];
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.kernelParams = ["i915.enable_dpcd_backlight=3"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
   services.fprintd.enable = lib.mkDefault true;
-
 }
